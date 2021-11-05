f17b5103436c67340a820bcd3afcbf19cc152944
olgasmirnova-1 <67696228+olgasmirnova-1@users.noreply.github.com>
2021-11-05 15:13:52 +0300

    Update test_items.py

--- test_items.py
+++ test_items.py
@@ -4,7 +4,7 @@ import time
 
 @pytest.mark.parametrize('language', ["es"])
 class TestLogin:
-    def test_guest_should_see_login_link(self, browser, language):
+    def test_find_button(self, browser, language):
         link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
         browser.get(link)
         button = len(browser.find_elements_by_css_selector("#add_to_basket_form > button"))        
